64893a67b78162f4f0c2a87ebc61c9c315f6abca
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-10 16:52:09 -0300

    [ :white_check_mark: ] | test: broker - publish to > topic [unit] (module::test)

--- test/unit/broker_test.go
+++ test/unit/broker_test.go
@@ -2,6 +2,7 @@ package unit_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Samuel-Ricardo/CapybaraMQ/internal/application"
 	"github.com/Samuel-Ricardo/CapybaraMQ/internal/domain/entity"
@@ -23,5 +24,27 @@ func TestSubscribe(t *testing.T) {
 
 	if len(topic.Subscribers) != 1 {
 		t.Fatalf("Expected 1 subscriber, got %d", len(topic.Subscribers))
+	}
+}
+
+func TestPublishEventToTopic(t *testing.T) {
+	broker := application.NewMessageBroker()
+	var received bool
+
+	handler := entity.NewEventHandler(func(e entity.Event) error {
+		received = true
+		return nil
+	})
+
+	broker.Subscribe("topic1", handler)
+	broker.Publish("topic1", entity.SampleEvent{Message: "Test Event"})
+
+	broker.ThreadGuard.RLock()
+	defer broker.ThreadGuard.RUnlock()
+
+	time.Sleep(2 * time.Second)
+
+	if !received {
+		t.Fatal("Expected event to be received by subscriber")
 	}
 }
