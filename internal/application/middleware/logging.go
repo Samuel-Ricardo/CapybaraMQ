4d2eb07fa505baf39f64acf32d64295d44712d41
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-11-10 13:51:31 -0300

    [ :sparkles: ] | create: middleware - logging (module::application)

---
+++ internal/application/middleware/logging.go
@@ -0,0 +1,14 @@
+package middleware
+
+import (
+	"log"
+
+	"github.com/Samuel-Ricardo/CapybaraMQ/internal/domain/entity"
+)
+
+func WithLogging() func(entity.Subscriber, entity.Event) error {
+	return func(s entity.Subscriber, e entity.Event) error {
+		log.Printf("Subscriber: %s, Event: %s", s, e)
+		return nil
+	}
+}
